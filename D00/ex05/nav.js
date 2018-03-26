71592015b4287e04f60377998499a109bc03fadf
Rizky Ario <rizkyario@yahoo.com>
2018-03-26 21:30:57 +0200

    Added Nav at ex05

---
+++ D00/ex05/nav.js
@@ -0,0 +1,23 @@
+function selectImg(elem) {
+    var img = document.getElementsByTagName('img');
+    for (i = 0; i < img.length; i++) {
+		img[i].classList.remove('selected')
+    }
+	elem.classList.add('selected');
+}
+function toggleSpeak() {
+    var x = document.getElementsByClassName("chat-section")[0];
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function goto(name){
+	var contents = document.getElementsByClassName("content");
+    for (i = 0; i < contents.length; i++) {
+		contents[i].style.display = "none";
+	}
+	var x = document.getElementsByClassName(name)[0];
+    x.style.display = "block";
+}
