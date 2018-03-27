c61a68820016e42eb7e8bfced77b884778c0f98c
Rizky Ario <rizkyario@yahoo.com>
2018-03-27 10:28:58 +0200

    Added D01/ex05

---
+++ D01/ex05/epur_str.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+	function ft_split($str){
+		$ret = array_filter(explode(' ', $str));
+		sort($ret);
+		return $ret;
+	}
+
+	if ($argc != 2)
+		return ;
+	$strtab = ft_split($argv[1]);
+	foreach($strtab as $key=>$str)
+	{
+		if ($key != 0)
+			echo " ";
+		echo $str;
+	}
+	echo "\n";
+?>
