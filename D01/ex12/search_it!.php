0b5466c77f66dd934f6133da5a935afdb821469e
Rizky Ario <rizkyario@yahoo.com>
2018-03-27 12:44:09 +0200

    Normed

--- D01/ex12/search_it!.php
+++ D01/ex12/search_it!.php
@@ -1,13 +1,12 @@
 #!/usr/bin/php
 <?php
 	if ($argc < 3)
-	{
         exit();
-    }
     $search = $argv[1];
 	unset($argv[0], $argv[1]);
 	$data = array();
-    foreach ($argv as $v){
+	foreach ($argv as $v)
+	{
 		$tmp = explode(":", $v);
 		if (count($tmp) == 2)
         	$data[$tmp[0]]  = $tmp[1] ;
