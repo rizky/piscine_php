6e4eb025b76dfd82e8173fe2aaebae9ec0f71c3e
Rizky Ario <rizkyario@yahoo.com>
2018-03-27 12:40:45 +0200

    Fixed bugs

--- D01/ex12/search_it!.php
+++ D01/ex12/search_it!.php
@@ -1,14 +1,16 @@
 #!/usr/bin/php
 <?php
-    if ($argc < 3) {
+	if ($argc < 3)
+	{
         exit();
     }
     $search = $argv[1];
 	unset($argv[0], $argv[1]);
 	$data = array();
     foreach ($argv as $v){
-        $tmp = explode(":", $v);
-        $data[$tmp[0]]  = $tmp[1] ;
+		$tmp = explode(":", $v);
+		if (count($tmp) == 2)
+        	$data[$tmp[0]]  = $tmp[1] ;
 	}
 	if (array_key_exists($search, $data))
 		echo $data[$search]."\n";
