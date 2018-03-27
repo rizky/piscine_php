e0a18824c762144f7da5f75d776b3491112dffac
Rizky Ario <rizkyario@yahoo.com>
2018-03-27 12:38:53 +0200

    Improved search_it!

--- D01/ex12/search_it!.php
+++ D01/ex12/search_it!.php
@@ -4,12 +4,12 @@
         exit();
     }
     $search = $argv[1];
-    unset($argv[0], $argv[1]);
+	unset($argv[0], $argv[1]);
+	$data = array();
     foreach ($argv as $v){
         $tmp = explode(":", $v);
-        if ($search === $tmp[0]){
-            echo $tmp[1]."\n";
-            exit();
-        }
-    }
+        $data[$tmp[0]]  = $tmp[1] ;
+	}
+	if (array_key_exists($search, $data))
+		echo $data[$search]."\n";
 ?>
