4f9ab6960eef3009fdee54e42c5dac98a932dfb1
Rizky Ario <rizkyario@yahoo.com>
2018-03-28 19:07:58 +0200

    Fixed bugs

--- D02/ex06/srt.php
+++ D02/ex06/srt.php
@@ -4,39 +4,30 @@
         return preg_match("/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]$/", $a);
     }
 
-    function is_sort($a, $b) {
-        return strcmp($a, $b);
-    }
-
-    function swap($time, $a, $b) {
-        $tmp = $time[$a];
-        $time[$a] = $time[$b];
-        $time[$b] = $tmp;
-    }
-
     if ($argc != 2 || !file_exists($argv[1]))
         exit();
-
     $read = fopen($argv[1], 'r');
-    while ($read && !feof($read))
-        $array[] = fgets($read);
+	while ($read && !feof($read))
+	{
+		$order = fgets($read);
+		echo $order;
+		$time = fgets($read);
+		echo $time;
+		if (!ft_is_time($time))
+		{
+			echo "Invalid File\n";
+			exit();
+		}
+		$array[$time] = fgets($read);
+		echo $time;
+	}
 
-    foreach($array as $k => $v){
-        if (ft_is_time($v)) {
-            $time[$k] = $v;
-        }
-    }
-
-    sort($time);
-
+    ksort($array);
     $index = 0;
-
-    foreach($array as $k => $v){
-        if (ft_is_time($v)) {
-            echo $time[$index];
-            $index++;
-        } else {
-            echo $v;
-        }
+    foreach($array as $k=>$v){
+		echo $index."\n";
+		echo $k."\n";
+		echo $v."\n";
+		$index++;
 	}
 ?>
