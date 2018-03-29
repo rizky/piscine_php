6458e359515cf14bd8a65c439bc600828bce8ce4
rizkyario <rizkyario@yahoo.com>
2018-03-29 22:30:31 +0200

    Restructured

--- D02/ex02/magnifying_glass.php
+++ D02/ex02/magnifying_glass.php
@@ -4,18 +4,20 @@
 		exit();
 	$read = fopen($argv[1], 'r');
 	$page = "";
-    while ($read && !feof($read)) {
+	while ($read && !feof($read))
+	{
         $page .= fgets($read);
     }
-    $page = preg_replace_callback("/(<a )(.*?)(>)(.*)(<\/a>)/is", function($matches) {
-        $matches[0] = preg_replace_callback("/( title=\")(.*?)(\")/is", function($matches) {
+	$page = preg_replace_callback("/(<a )(.*?)(>)(.*)(<\/a>)/is", function($matches)
+	{
+		$matches[0] = preg_replace_callback("/( title=\")(.*?)(\")/is", function($matches)
+		{
             return ($matches[1]."".strtoupper($matches[2])."".$matches[3]);
         }, $matches[0]);
-
-        $matches[0] = preg_replace_callback("/(>)(.*?)(<)/is", function($matches) {
+		$matches[0] = preg_replace_callback("/(>)(.*?)(<)/is", function($matches) 
+		{
             return ($matches[1]."".strtoupper($matches[2])."".$matches[3]);
         }, $matches[0]);
-
         return ($matches[0]);
     }, $page);
     echo $page;
