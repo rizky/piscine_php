7fecca139b597ca6964fcdd08f4e623b22fa6636
rizkyario <rizkyario@yahoo.com>
2018-03-29 22:29:22 +0200

    Restructured

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -2,13 +2,15 @@
 <?php
     date_default_timezone_set('Europe/Paris');
     $file = fopen("/var/run/utmpx", "r");
-    while ($utmpx = fread($file, 628)){
+	while ($utmpx = fread($file, 628))
+	{
         $unpack = unpack("a256a/a4b/a32c/id/ie/I2f", $utmpx);
-        $array[$unpack['c']] = $unpack;
+        $array[] = $unpack;
     }
-    ksort($array);
-    foreach ($array as $v){
-        if ($v['e'] == 7) {
+	foreach ($array as $v)
+	{
+		if ($v['e'] == 7)
+		{
             echo str_pad(substr(trim($v['a']), 0, 8), 8, " ")." ";
             echo str_pad(substr(trim($v['c']), 0, 8), 8, " ")." ";
             echo date("M", $v["f1"]);
