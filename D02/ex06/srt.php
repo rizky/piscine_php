ff72f338c8afe7c4aa1b2fc121cd1756bde99575
rizkyario <rizkyario@yahoo.com>
2018-03-29 22:51:58 +0200

    Improved srt.php

--- D02/ex06/srt.php
+++ D02/ex06/srt.php
@@ -13,15 +13,25 @@
 
     if ($argc != 2 || !file_exists($argv[1]))
         exit();
-    $read = fopen($argv[1], 'r');
+	$read = fopen($argv[1], 'r');
+	$array = array();
 	while ($read && !feof($read))
 	{
 		$order = fgets($read);
 		$time = str_replace("\n", "", fgets($read));
 		if (!ft_is_time($time))
 			ft_error();
-		$array[$time] = str_replace("\n", "", fgets($read));
-		$nl = fgets($read);
+		$text = fgets($read);
+		$i = 0;
+		while ($text && strcmp($text, "\n") != 0)
+		{
+			if ($i == 0)
+				$array[$time] = str_replace("\n", "", $text);
+			else
+				$array[$time] = $array[$time]."\n".str_replace("\n", "", $text);
+			$text = fgets($read);
+			$i++;
+		}
 	}
     ksort($array);
     $index = 1;
