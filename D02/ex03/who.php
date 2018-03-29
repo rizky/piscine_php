5b8849c9f08642cb427610ba4ec47a5544c3ca70
rizkyario <rizkyario@yahoo.com>
2018-03-29 22:25:58 +0200

    Improved code

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -3,7 +3,7 @@
     date_default_timezone_set('Europe/Paris');
     $file = fopen("/var/run/utmpx", "r");
     while ($utmpx = fread($file, 628)){
-        $unpack = unpack("a256a/a4b/a32c/id/ie/I2f/a256g/i16h", $utmpx);
+        $unpack = unpack("a256a/a4b/a32c/id/ie/I2f", $utmpx);
         $array[$unpack['c']] = $unpack;
     }
     ksort($array);
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM php:7.0-apache
 COPY . /var/www/html/
 
-# docker build -t rizkyario/piscine_php .
-# docker push rizkyario/piscine_php
+# docker build -t rizkyario/piscine_php_rush00 .
+# docker push rizkyario/piscine_php_rush00
