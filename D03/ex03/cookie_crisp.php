62b7cb60e417ea85522f2e91a850a10600adc565
Rizky Ario <rizkyario@yahoo.com>
2018-03-30 11:03:14 +0200

    Normed

--- D03/ex03/cookie_crisp.php
+++ D03/ex03/cookie_crisp.php
@@ -1,16 +1,17 @@
 <?php
-    switch ($_GET['action']) {
-	case("set"):
-		if ($_GET['name'] && $_GET['value'])
-                setcookie($_GET['name'], $_GET['value']);
-            break;
-		case("get"):
-				if ($_GET['name'] && $_COOKIE[$_GET['name']] && !$_GET['value'])
-                	echo $_COOKIE[$_GET['name']]."\n";
-            break;
-		case("del"):
-			if ($_GET['name'] && !$_GET['value'])
-            setcookie($_GET['name'], '', 1);
-            break;
+	switch ($_GET['action'])
+	{
+		case("set"):
+			if ($_GET['name'] && $_GET['value'])
+					setcookie($_GET['name'], $_GET['value']);
+				break;
+			case("get"):
+					if ($_GET['name'] && $_COOKIE[$_GET['name']] && !$_GET['value'])
+						echo $_COOKIE[$_GET['name']]."\n";
+				break;
+			case("del"):
+				if ($_GET['name'] && !$_GET['value'])
+				setcookie($_GET['name'], '', 1);
+				break;
     }
 ?>
