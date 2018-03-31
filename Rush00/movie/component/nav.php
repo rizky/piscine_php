8bc2b0384d403d8ecd0b038c433d05d0d7bed475
Rizky Ario <rizkyario@yahoo.com>
2018-03-31 07:52:36 +0200

    Change title

--- Rush00/movie/component/nav.php
+++ Rush00/movie/component/nav.php
@@ -5,7 +5,7 @@ $cartCount = isset($_SESSION['cartCount']) ? $_SESSION['cartCount']  : "Empty";
 <ul class="nav">
 	<li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
 	<li><a href="browse.php"><i class="fa fa-film"></i> Movies</a></li>
-	<li class="nav_logo">Mooflix</li>
+	<li class="nav_logo">Movlix</li>
 	<li><a href="cart.php"><i class="fa fa-shopping-cart"></i> <? echo $cartCount." Cart";?></a></li>
 	<? 
 	if (isset($_SESSION['pseudo']) && !empty($_SESSION['pseudo']))
