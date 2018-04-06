dd05c2631831fecaa0b1c2194b23b04e00a86686
Rizky Ario <rizkyario@yahoo.com>
2018-04-07 01:12:32 +0200

    Fixed delete todolist

--- D09/ex04/delete.php
+++ D09/ex04/delete.php
@@ -1,7 +1,7 @@
 <?php
     $list = '';
     $max = 0;
-    if (file_exists("list.csv") && isset($_GET["id"]) && !empty($_GET["id"]))
+    if (file_exists("list.csv") && isset($_GET["id"]))
     {
         $file = file("list.csv", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($file as $line) {
