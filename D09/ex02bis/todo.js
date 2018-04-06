36fb12e6537da91e0bd10610339683b09481116e
Rizky Ario <rizkyario@yahoo.com>
2018-04-06 23:45:24 +0200

    Added D09

---
+++ D09/ex02bis/todo.js
@@ -0,0 +1,40 @@
+var ft_list;
+var cookie = [];
+
+$(document).ready(function(){
+    $('#new').click(newTodo);
+    $('#ft_list div').click(deleteTodo);
+    ft_list = $('#ft_list');
+    var tmp = document.cookie;
+    if (tmp) {
+        cookie = JSON.parse(tmp);
+        cookie.forEach(function (e) {
+            addTodo(e);
+        });
+    }
+});
+
+$(window).unload(function(){
+    var todo = ft_list.children();
+    var newCookie = [];
+    for (var i = 0; i < todo.length; i++)
+        newCookie.unshift(todo[i].innerHTML);
+    document.cookie = JSON.stringify(newCookie);
+})
+
+function newTodo(){
+    var todo = prompt("What do you want to do?", '');
+    if (todo !== '') {
+        addTodo(todo)
+    }
+}
+
+function addTodo(todo){
+    ft_list.prepend($('<div>' + todo + '</div>').click(deleteTodo));
+}
+
+function deleteTodo(){
+    if (confirm("Are you sure you want to delete this task?")){
+        this.remove();
+    }
+}
